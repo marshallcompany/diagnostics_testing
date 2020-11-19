4dbd9538d86a7d1dd62b6eaf0bd39cafc697e3c6
Vitaliy Stepanenko <vstepanenkotrash1994@gmail.com>
2020-11-19 21:47:03 +0200

    Initialization diagnostics testing

---
+++ gulpfile.js
@@ -0,0 +1,81 @@
+const { src, dest, parallel, series, watch } = require('gulp');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+const notify = require('gulp-notify');
+const imagesmin = require('gulp-imagemin');
+const del = require('del');
+const paths = [
+    {
+        src: './src/sass/pages/**/*.scss',
+        dest: './src/pages'
+    },
+    {
+        src: './src/sass/*.scss',
+        dest: './src'
+    }
+];
+// Browser-sync
+const initBrowserSync = () => {
+    browserSync.init({
+        server: {
+            baseDir: './src'
+        },
+        ui: {
+            port: 8080
+        },
+        port: 8000,
+        online: true
+    });
+    watch('./src/**/*.scss').on('change', parallel(styles, browserSync.reload));
+    watch('./src/**/*.html').on('change', browserSync.reload);
+    watch('./src/**/*.js').on('change', browserSync.reload);
+    watch('./src/**/*.php').on('change', browserSync.reload);
+}
+// Styles
+const styles = (cb) => {
+    cb();
+    paths.forEach(patch => {
+        return src(patch.src)
+        .pipe(sass())
+        .on('error', notify.onError({
+            message: 'Error: <%= error.message %>',
+            title: 'Error'
+        }))
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['last 10 versions'],
+            grid: true,
+            cascade: false
+        }))
+        .pipe(dest(patch.dest))
+    });
+};
+// Images minification
+const imagesMinification = () => {
+    return src('./src/**/*.+(jpg|png|ico|svg)', { base: './src' })
+    .pipe(imagesmin())
+    .pipe(dest('./dist'))
+};
+// Delete dir dist
+const deleteDist = () => {
+    return del(['./dist']);
+};
+// Copy file
+const copyFile = () => {
+    return src([
+        './src/libs/**/*.js',
+        './src/libs/**/*.php',
+        './src/libs/**/*.css',
+        './src/libs/**/*.map',
+        './src/**/*.html',
+        './src/**/*.css',
+        './src/**/*.js',
+        './src/**/*.+(otf|ttf|woff|woff2|eot)',
+        './src/**/*.+(mov|mp4)'
+    ], { base: './src' })
+    .pipe(dest('./dist'));
+};
+// Global
+exports.deleteDist = deleteDist,
+exports.build = series(deleteDist, styles, copyFile, imagesMinification);
+exports.watch = initBrowserSync;
---
+++ src/pages/testing/main.js
@@ -0,0 +1,142 @@
+'use strict';
+
+let testData = {
+    bookkeeping: new Array(),
+    taxes: new Array(),
+    management: new Array(),
+    legal: new Array()
+};
+
+let sliderIndex = 0;
+const sliderItem = document.querySelectorAll('.test-item');
+const sliderTabs = document.querySelectorAll('.tabs .tabs__item');
+const inputsRadio = document.querySelectorAll('input[type="radio"]');
+const sliderPrevButton =  document.querySelector('.test-navigation__prev-button');
+const sliderNextButton =  document.querySelector('.test-navigation__next-button');
+const testResultButton = document.querySelector('.test-navigation__result-button');
+
+
+inputsRadio.forEach(input => {
+    input.addEventListener('change', (element) => {
+        const groupName = element.target.parentNode.parentNode.parentNode.dataset['group'];;
+        const inputValue = element.target.value;
+        sliderIndex === (sliderItem.length - 1) ? testResultButton.removeAttribute('disabled') : 
+        sliderNextButton.removeAttribute('disabled');
+        updateTestData(groupName, inputValue);
+    });
+});
+
+function updateTestData(groupName, value) {
+    const sliderItem = document.querySelectorAll('.test-item .test-item__question');
+    testData[groupName].push({
+        question: sliderItem[sliderIndex].innerHTML.trim(),
+        answer: value
+    });
+}
+
+// SLIDER TEST NAVIGATION
+sliderPrevButton.style = `
+        opacity: 0;
+        pointer-events: none;
+    `;
+
+function sliderPrev() {
+    if (sliderIndex !== 0) {
+        sliderIndex--;
+        sliderItem.forEach(item => {
+            item.style.display = 'none';
+        })
+        sliderItem[sliderIndex].style.display = 'block';
+        checkStates(sliderItem[sliderIndex].dataset['group']);
+    }
+}
+
+function sliderNext() {
+    if (sliderIndex < sliderItem.length - 1) {
+        sliderIndex++;
+        sliderItem.forEach(item => {
+            item.style.display = 'none';
+        })
+        sliderItem[sliderIndex].style.display = 'block';
+        checkStates(sliderItem[sliderIndex].dataset['group']);
+    }
+}
+document.querySelector('.step .step__full').innerHTML = ` / ${sliderItem.length}`;
+function checkStates(nameStates) {
+    document.querySelector('.step .step__active').innerHTML = `${sliderIndex + 1}`
+    // DISABLED - ACTIVE TABS
+    sliderTabs.forEach(item => {
+        if (item.dataset['group'] === nameStates) {
+            item.classList.add('active');            
+        } else {
+            item.classList.remove('active');
+        }
+    })
+    // DISABLED - ACTIVE BUTTON
+    for(let item of inputsRadio) {
+        if (sliderItem[sliderIndex].contains(item)) {
+            if (item.checked) {
+                sliderNextButton.removeAttribute('disabled');
+                break;
+            } else {
+                sliderNextButton.setAttribute('disabled', '');
+            }
+        }
+    }
+    switch(sliderIndex) {
+        case 0:
+            sliderPrevButton.style = `
+                opacity: 0;
+                pointer-events: none;
+            `;
+            break;
+        case sliderItem.length - 1:
+            sliderNextButton.style.display = 'none';
+            testResultButton.style.display = 'block';
+            break;
+        default:
+            testResultButton.style.display = 'none';
+            sliderNextButton.style.display = 'block';
+            sliderPrevButton.style = `
+                opacity: 1;
+                pointer-events: initial;
+            `;
+            break;
+    };
+}
+
+function getTestingResult() {
+    console.log('[ TESTING RESULT ]', testData);
+}
+
+// GENERATION - DOWNLOAD TEST RESULPR
+function resultTestDownload() {
+    const data = document.body.querySelector('#capture');
+    document.querySelector('body').classList.add('hide-scrollbar');
+    html2canvas(data, {
+        scrollX: 0,
+        scrollY: -window.scrollY,
+        width:  data.clientWidth , 
+        height: data.clientHeight,
+    })
+    .then(canvas => {
+        document.querySelector('body').classList.remove('hide-scrollbar');
+        saveAs(canvas.toDataURL(), 'diagnostics-test.png');
+    }) 
+    .catch(error => {
+        console.log('[ GENERATION - DOWNLOAD TEST RESULT ERROR ]', error);
+    })
+}
+
+function saveAs(url, filename) {
+    let link = document.createElement('a');
+    if (typeof link.download === 'string') {
+        link.href = url;
+        link.download = filename;
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    } else {
+        window.open(url);
+    }
+}
